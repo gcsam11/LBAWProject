e4248d5cc3a6a80c20903f7415da401022c88c69
AndreSantos03 <andrebernardosantos@gmail.com>
2023-10-20 14:01:10 +0100

    changes

--- db.sql
+++ db.sql
@@ -1,6 +1,18 @@
-CREATE TABLE users(
-    name VARCHAR (50) NOT NULL,
-    username VARCHAR (50) PRIMARY KEY,
+CREATE TABLE accounts (
+	user_id serial PRIMARY KEY,
+	username VARCHAR ( 50 ) UNIQUE NOT NULL,
+	password VARCHAR ( 50 ) NOT NULL,
+	email VARCHAR ( 255 ) UNIQUE NOT NULL,
+	created_on TIMESTAMP NOT NULL,
+        last_login TIMESTAMP 
+);
+
+
+CREATE TABLE users (
+    id serial PRIMARY KEY,
+    
+    name text NOT NULL,
+    username VARCHAR ( 50 ),
     birthday DATE,
     country VARCHAR (50),
     city VARCHAR (50),
@@ -27,12 +39,13 @@ CREATE TABLE admin(
 );
 
 CREATE TABLE post(
+    post_id INTEGER,
     title VARCHAR (50),
     caption VARCHAR (50),
     postdate DATE,
-    
+    upvotes INTEGER,
+    downvotes INTEGER,
 );
-
 CREATE TABLE (
 
 );
