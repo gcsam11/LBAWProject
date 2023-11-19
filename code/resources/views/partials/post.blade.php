ecfde5dcb6bc9334a89e4c2bc90f18f4f30ae320
AndreSantos03 <andrebernardosantos@gmail.com>
2023-11-19 15:02:07 +0000

    Added more information to each post

--- code/resources/views/partials/post.blade.php
+++ code/resources/views/partials/post.blade.php
@@ -1,13 +1,17 @@
 <article class="post" data-id="{{ $post->id }}">
     <header>
-        <h2>{{ $post->title }}</h2>
+        <h2>Title:{{ $post->title }}</h2>
         <a href="#" class="delete">&#10761;</a>
     </header>
     <ul>
         <!-- Display post details or any specific information -->
-        <li>{{ $post->caption }}</li>
-        <li>Posted by: {{ $post->user->name }}</li> <!-- Assuming you have a user relationship -->
-        <!-- Display other relevant post details -->
+        <li><strong>Caption:</strong> {{ $post->caption }}</li>
+        <li><strong>postdate:</strong> {{ $post->postdate }}</li>
+        <li><strong>upvotes:</strong> {{ $post->upvotes }}</li>
+        <li><strong>downvotes:</strong> {{ $post->downvotes }}</li>
+        <li><strong>user_id:</strong> {{ $post->user_id }}</li>
+        <li><strong>topic_id:</strong> {{ $post->topic_id }}</li>
+
     </ul>
     <form class="new_item">
         <input type="text" name="description" placeholder="new item">
