d259fe61c6b7de04b1a3242409bb642332ae5bda
Diogo-Silveira-Viana <diogosilveiraviana8@gmail.com>
2023-11-19 12:27:19 +0000

    logout working

--- code/resources/views/pages/profile.blade.php
+++ code/resources/views/pages/profile.blade.php
@@ -10,4 +10,8 @@
 
 @section('content')
     <!-- Add content for profile page -->
+
+    <form method="GET" action="{{ route('logout') }}">
+        <button type="submit">Logout</button>
+    </form>
 @endsection
