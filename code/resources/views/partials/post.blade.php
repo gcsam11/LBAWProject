e2f1b62a07267fa68f7dfc802d2a0013ea3cf4ae
Diogo-Silveira-Viana <diogosilveiraviana8@gmail.com>
2023-11-20 13:46:29 +0000

    topic_id & user_id removed from posts

--- code/resources/views/partials/post.blade.php
+++ code/resources/views/partials/post.blade.php
@@ -6,11 +6,9 @@
     <ul>
         <!-- Display post details or any specific information -->
         <li><strong>topic:</strong> {{ $post->topic->title }}</li>
-        <!-- Delete later --> <li><strong>topic_id:</strong> {{ $post->topic_id }}</li>
         <li><strong>Caption:</strong> {{ $post->caption }}</li>
         <li><strong>postdate:</strong> {{ $post->postdate }}</li>
         <li><strong>upvotes:</strong> {{ $post->upvotes }}</li>
         <li><strong>downvotes:</strong> {{ $post->downvotes }}</li>
-        <!-- Delete later --> <li><strong>user_id:</strong> {{ $post->user_id }}</li>
     </ul>
 </article>
