f8ca3e31ad1b8d228ed2fe96c61e1a7752b21310
Diogo-Silveira-Viana <diogosilveiraviana8@gmail.com>
2023-11-20 15:10:30 +0000

    changed a little bit the posts and the comments

--- code/resources/views/pages/comments.blade.php
+++ code/resources/views/pages/comments.blade.php
@@ -3,12 +3,12 @@
     <ul>
         @foreach ($comments as $comment)
             <li>
-                <p>Title: {{ $comment->title }}</p>
-                <p>Caption: {{ $comment->caption }}</p>
-                <p>Comment Date: {{ $comment->commentdate }}</p>
-                <p>Upvotes: {{ $comment->upvotes }}</p>
-                <p>Downvotes: {{ $comment->downvotes }}</p>
-                <p>Posted by: {{ $comment->user->name }}</p>
+                <p><strong>Title:</strong> {{ $comment->title }}</p>
+                <p><strong>Caption:</strong> {{ $comment->caption }}</p>
+                <p><strong>Comment Date:</strong> {{ $comment->commentdate }}</p>
+                <p><strong>Upvotes:</strong> {{ $comment->upvotes }}</p>
+                <p><strong>Downvotes:</strong> {{ $comment->downvotes }}</p>
+                <p><strong>Posted by:</strong> {{ $comment->user->name }}</p>
             </li>
         @endforeach
     </ul>
--- code/resources/views/partials/post.blade.php
+++ code/resources/views/partials/post.blade.php
@@ -1,14 +1,15 @@
 <article class="post" data-id="{{ $post->id }}">
     <header>
-        <h2>Title:{{ $post->title }}</h2>
+        <h2>{{ $post->title }}</h2>
         <a href="#" class="delete">&#10761;</a>
     </header>
     <ul>
         <!-- Display post details or any specific information -->
-        <li><strong>topic:</strong> {{ $post->topic->title }}</li>
+        <li><strong>Topic:</strong> {{ $post->topic->title }}</li>
         <li><strong>Caption:</strong> {{ $post->caption }}</li>
-        <li><strong>postdate:</strong> {{ $post->postdate }}</li>
-        <li><strong>upvotes:</strong> {{ $post->upvotes }}</li>
-        <li><strong>downvotes:</strong> {{ $post->downvotes }}</li>
+        <li><strong>Date:</strong> {{ $post->postdate }}</li>
+        <li><strong>Upvotes:</strong> {{ $post->upvotes }}</li>
+        <li><strong>Downvotes:</strong> {{ $post->downvotes }}</li>
+        <li><strong>Posted by:</strong> {{ $post->user->name }}</li>
     </ul>
 </article>
