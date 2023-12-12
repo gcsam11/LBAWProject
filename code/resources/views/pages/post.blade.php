41a225be712ab0c948a31cc0742d73ffaa82559f
AndreSantos03 <andrebernardosantos@gmail.com>
2023-12-12 10:30:58 +0000

    Quick fix to post display n page for psot

--- code/resources/views/pages/post.blade.php
+++ code/resources/views/pages/post.blade.php
@@ -4,7 +4,16 @@
 
 @section('content')
     <section id="posts">
-        @include('partials.posts', ['post' => $post])
+        <header>
+            <h2>{{ $post->title }}</h2>
+        </header>
+        <!-- Display post details or any specific information -->
+        <strong>Topic:</strong> {{ $post->topic->title }}<br><br>
+        <strong>Caption:</strong> {{ $post->caption }}<br><br>
+        <strong>Date:</strong> {{ $post->postdate }}<br><br>
+        <strong>Upvotes:</strong> {{ $post->upvotes }}<br><br>
+        <strong>Downvotes:</strong> {{ $post->downvotes }}<br><br>
+        <strong>Posted by:</strong> <a href="{{ route('profile_page', ['id' => $post->user->id]) }}">{{ $post->user->name }}</a>
         @include('pages.comments', ['comments' => $comments])
     </section>
     {{-- Update Button --}}
