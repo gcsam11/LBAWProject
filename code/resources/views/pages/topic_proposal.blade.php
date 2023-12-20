78e3c9c72d47029590e700c4d77666d0d76a034f
Diogo-Silveira-Viana <diogosilveiraviana8@gmail.com>
2023-12-20 21:45:47 +0000

    added a few more placeholders

--- code/resources/views/pages/topic_proposal.blade.php
+++ code/resources/views/pages/topic_proposal.blade.php
@@ -14,12 +14,12 @@
         <form method="POST" action="{{ route('createTopicProposal') }}">
             @csrf
             <div>
-                <label for="topic_name">Topic Proposal Name<span>*</span></label>
-                <input type="text" id="topic_name" name="topic_name" placeholder="Topic Name" required>
+                <label for="topic_name">Topic Name<span>*</span></label>
+                <input type="text" id="topic_name" name="topic_name" placeholder="e.g. Lorem Ipsum" required>
             </div>
             <div>
                 <label for="caption">Caption</label>
-                <textarea id="caption" name="caption" placeholder="Message" required></textarea>
+                <textarea id="caption" name="caption" placeholder="e.g. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at pellentesque lectus, id consectetur nunc." required></textarea>
             </div>
             <div>
                 <button type="submit">Submit</button>
